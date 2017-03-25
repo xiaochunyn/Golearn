9b1b974c3a772d2abc7e2d7bfeb5556992416e9b
xiaochunyn <1481839020@qq.com>
2017-03-25 10:55:43 +0800

    update insertSort.go

--- sort/insertSort.go
+++ sort/insertSort.go
@@ -30,3 +30,32 @@ func insertSort(a []int) []int {
 	}
 	return a
 }
+
+
+
+beta.kubernetes.io/arch=amd64,   beta.kubernetes.io/instance-type=rancher,
+beta.kubernetes.io/os=linux,
+
+failure-domain.beta.kubernetes.io/region=Region1,
+failure-domain.beta.kubernetes.io/zone=FailureDomain1,
+
+io.rancher.host.docker_version=1.12,
+io.rancher.host.linux_kernel_version=3.13,
+
+kubernetes.io/hostname=node,orchestration=true
+
+
+
+beta.kubernetes.io/arch=amd64,
+beta.kubernetes.io/os=linux,
+kubernetes.io/hostname=10.10.101.83
+
+
+beta.kubernetes.io/instance-type=n1-standard-2,
+
+failure-domain.beta.kubernetes.io/region=hc-central1,
+failure-domain.beta.kubernetes.io/zone=hc-central1-a,
+
+kubernetes.io/hostname=kubernetes-minion-9vlv
+
+kubectl label nodes 10.10.101.83 failure-domain.beta.kubernetes.io/region=hc-central1 failure-domain.beta.kubernetes.io/zone=hc-central1-a
